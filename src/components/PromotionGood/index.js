0ced7a3944e589d6e18293bcc50991ef954351c7
lilu <787688238@qq.com>
2020-03-24 21:06:31 +0800

    更新

--- src/components/PromotionGood/index.js
+++ src/components/PromotionGood/index.js
@@ -2,32 +2,33 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const PromotionGood = ({ benefit }) => {
-	return (
-		benefit.length > 0 &&
-		<div className="promotionGood">
-			{
-				benefit.map((item, index) => (
-					<Link className="item acea-row row-between-wrapper" key={index} to={'detail/' + item.id}>
-						<div className="pictrue">
-							<img src={item.image} className="image" alt="" />
-						</div>
-						<div className="text">
-							<div className="name line1">{item.store_name}</div>
-							<div className="sp-money acea-row">
-								<div className="moneyCon">
-									促销价: ￥<span className="num">{item.price}</span>
-								</div>
-							</div>
-							<div className="acea-row row-between-wrapper">
-								<div className="money">日常价：￥{item.ot_price}</div>
-								<div>仅剩：{item.stock}{item.unit_name}</div>
-							</div>
-						</div>
-					</Link>
-				))
-			}
-		</div>
-	)
+
+  return (
+    benefit.length > 0 &&
+    <div className="promotionGood">
+      {
+        benefit.map((item, index) => (
+          <Link className="item acea-row row-between-wrapper" key={index} to={'detail/' + item.id}>
+            <div className="pictrue">
+              <img src={item.image} className="image" alt="" />
+            </div>
+            <div className="text">
+              <div className="name line1">{item.store_name}</div>
+              <div className="sp-money acea-row">
+                <div className="moneyCon">
+                  促销价: ￥<span className="num">{item.price}</span>
+                </div>
+              </div>
+              <div className="acea-row row-between-wrapper">
+                <div className="money">日常价：￥{item.ot_price}</div>
+                <div>仅剩：{item.stock}{item.unit_name}</div>
+              </div>
+            </div>
+          </Link>
+        ))
+      }
+    </div>
+  )
 }
 
 export default PromotionGood
--- src/libs/login.js
+++ src/libs/login.js
@@ -1,5 +1,6 @@
 import store from '@/store'
 import history from '../config/history'
+import cookie from '../utils/store/cookie'
 export function toLogin( obj) {
 	// 没有token 跳转到登陆页面
   store.dispatch({
@@ -13,12 +14,14 @@ export function toLogin( obj) {
 
 export function toMainLogin() {
   console.log('未登录')
+  store.dispatch({
+    type: 'LOGOUT'
+  })
+  cookie.set('login_back_url',`${history.location.pathname}${history.location.search}`)
+
   // token过期
   // 重定向到登陆页面
-  history.push({
-    pathname: '/login',
-    state: { from: history.location }
-  })
+  history.push('/login')
   return
 }
 
